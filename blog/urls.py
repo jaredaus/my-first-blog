0d4aca8a65a6a5cf110640598c610139447d61e3
Jared <jared.hammond.aus@gmail.com>
2017-05-17 19:14:50 +0930

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'), #This regular expression will match ^ (a beginning) followed by $ (an end) – so only an empty string will match
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
